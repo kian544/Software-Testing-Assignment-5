586615981be8f5001c3a1190040d763463652f53
kian544 <147429923+kian544@users.noreply.github.com>
2025-02-25 00:36:45 -0600

    Kian Kakavandi - Improve Testability and stubbed dependancy
    Part 3.b

--- FilterInvoice.java
+++ FilterInvoice.java
@@ -1,39 +1,41 @@
 package org.example.Assignment;
 
 import org.junit.Test;
-
 import java.util.List;
 import static java.util.stream.Collectors.toList;
 import static org.junit.Assert.assertTrue;
-
+import java.util.Arrays;
 
 public class FilterInvoice {
-    QueryInvoicesDAO dao;
-    Database db;
+    private QueryInvoicesDAO dao;
+    public void setDao(QueryInvoicesDAO dao) { //Setter for dependency injection
 
-    // We want to stub the dao to avoid interacting with database, however it is hard to do so, since dao is initialized internally
-    // we need some way to inject dependency which is a stub, so we don't interact with database explicitly
-    // we want it to depend on concretion, but only an abstraction.
-    public FilterInvoice() {
-        // this class doesn't need db, only dao needs it... there is a tight coupling
-        // this is called dependency instantiation not injection
-        this.db = new Database();
-        this.dao = new QueryInvoicesDAO(db);
+        this.dao = dao;
     }
-
     public List<Invoice> lowValueInvoices() {
         List<Invoice> all = dao.all();
-
         return all.stream()
                 .filter(invoice -> invoice.getValue() < 100)
                 .collect(toList());
     }
     @Test
-    public void filterInvoiceTest(){
-        Database db = new Database();
-        QueryInvoicesDAO dao = new QueryInvoicesDAO(db);
-        FilterInvoice f = new FilterInvoice();
+    public void filterInvoiceStubbedTest() {
+        QueryInvoicesDAO stubDao = new QueryInvoicesDAO(null) { //Stubbed DAO to return test data
+            @Override
+            public List<Invoice> all() {
+                return Arrays.asList(
+                        new Invoice("INV-001", 50),
+                        new Invoice("INV-002", 75),
+                        new Invoice("INV-003", 125) // Should be filtered out due to OOB
+                );
+            }
+        };
+        FilterInvoice f = new FilterInvoice(); //Inject stubbed DAO
+        f.setDao(stubDao);  // Injecting the stub
+
         List<Invoice> result = f.lowValueInvoices();
-        assertTrue(result.stream().allMatch(invoice -> invoice.getValue()<100));
+
+
+        assertTrue(result.stream().allMatch(invoice -> invoice.getValue() < 100));//Make sure invoices have a value less than 100
     }
 }
